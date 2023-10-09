62ff59cbdf97401ac18d52ac156fc023e8409050
Alexandr Romanchuk <alexandr.romanchuk96@gmail.com>
2023-10-09 21:56:59 +0300

    Added client layout component

---
+++ app/components/Layout/layout.tsx
@@ -0,0 +1,101 @@
+'use client';
+import './layout.scss';
+import { ReactNode, useEffect, useRef, useState } from 'react';
+import Home from '@/app/page';
+import AboutTeamPage from '@/app/about-team/page';
+import Header from '@/components/Header';
+import { m, AnimatePresence, useInView, LazyMotion, domAnimation } from 'framer-motion';
+import { usePathname } from 'next/navigation';
+import Footer from '@/components/Footer';
+import SolutionsPage from '@/app/solutions/page';
+
+export default function RootClientLayout({ children }: { children: ReactNode }) {
+  const footerRef = useRef<HTMLDivElement>(null);
+  const pageWrpRef = useRef<HTMLDivElement>(null);
+  const markRef = useRef<HTMLDivElement>(null);
+  const markInView = useInView(markRef);
+  const pathName = usePathname();
+  const [isStartAnimation, setIsStartAnimation] = useState(false);
+
+  const routes = [
+    { path: '/', component: <Home /> },
+    { path: '/about-team', component: <AboutTeamPage /> },
+    { path: '/solutions', component: <SolutionsPage /> },
+  ];
+
+  const animatedRoutes = routes.map(route => route.path);
+
+  useEffect(() => {
+    if (pageWrpRef.current) {
+      pageWrpRef.current.scroll(0, 0);
+    }
+    window.scroll(0, 0);
+  }, [pathName]);
+
+  const variants = {
+    initial: {
+      y: '100vh',
+      borderRadius: '30px',
+      transition: {
+        duration: 0,
+      },
+    },
+    animate: {
+      borderRadius: 0,
+      y: 0,
+      transition: {
+        y: { duration: 0.5 },
+        borderRadius: { duration: 1, delay: 0.3 },
+      },
+    },
+    exit: {
+      opacity: 0.1,
+      borderRadius: '30px',
+      y: '70px',
+      scale: 0.9,
+      transition: {
+        ease: 'easeInOut',
+        scale: { duration: 0.7 },
+        borderRadius: { duration: 0 },
+      },
+    },
+  };
+
+  return (
+    <>
+      <div
+        ref={pageWrpRef}
+        className={`page-wrp ${isStartAnimation ? 'page-wrp--hidden' : ''} ${
+          markInView ? 'page-wrp--view-footer' : ''
+        }`}
+      >
+        <LazyMotion features={domAnimation}>
+          <AnimatePresence initial={false} mode={'popLayout'}>
+            {animatedRoutes.includes(pathName) ? (
+              <m.main
+                key={pathName}
+                variants={variants}
+                initial="initial"
+                animate={'animate'}
+                exit={'exit'}
+                className={'main'}
+                onAnimationStart={() => setIsStartAnimation(true)}
+                onAnimationComplete={() => setIsStartAnimation(false)}
+              >
+                <Header />
+                {routes.find(route => route.path === pathName)?.component}
+              </m.main>
+            ) : (
+              <main className={'main'}>
+                <Header />
+                {children}
+              </main>
+            )}
+          </AnimatePresence>
+        </LazyMotion>
+        <div ref={markRef} className={'page-wrp__mark'}></div>
+      </div>
+      <Footer ref={footerRef} isVisible={markInView} />
+    </>
+  );
+}
