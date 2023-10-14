7aaa46888748b5fe508261c357305b489d425951
Alexandr Romanchuk <alexandr.romanchuk96@gmail.com>
2023-10-14 21:39:08 +0300

    Added founders data

---
+++ core/constants/founders.ts
@@ -0,0 +1,13 @@
+export const founder_1 = {
+  imagePath: '/founders/founder_1.jpeg',
+  telegram: 'https://t.me/German_Bobnev',
+  fullName: 'Герман Бобнев',
+  responsible: 'Отвечает за коммуникацию и менеджмент ',
+};
+
+export const founder_2 = {
+  imagePath: '/founders/founder_2.jpeg',
+  telegram: 'https://t.me/dandotsenko',
+  fullName: 'Данил Доценко',
+  responsible: 'Отвечает за команду, дизайн и разработку',
+};
