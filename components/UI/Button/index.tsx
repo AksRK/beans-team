a72c006a3fa17acedc933d5dba0ef2b1a1af1e74
Alexandr Romanchuk <alexandr.romanchuk96@gmail.com>
2023-10-26 19:03:05 +0300

    Added OnClick, whileTapAnimation, type props to Button component

--- components/UI/Button/index.tsx
+++ components/UI/Button/index.tsx
@@ -7,16 +7,27 @@ interface IButton {
   children: ReactNode;
   fullWidth?: boolean;
   color?: 'primary' | 'dark';
+  onClick?: () => void;
+  whileTapAnimation?: boolean;
+  type?: 'button' | 'submit' | 'reset';
 }
 
-const Button: FC<IButton> = ({ children, fullWidth = false, color = 'primary' }) => {
+const Button: FC<IButton> = ({
+  children,
+  fullWidth = false,
+  color = 'primary',
+  onClick,
+  whileTapAnimation = true,
+  type = 'button',
+}) => {
   const buttonColor = {
     primary: ' button--color-primary',
     dark: ' button--color-dark',
   };
   const buttonClassNames = ['button', buttonColor[color], fullWidth ? ' button--full-width' : ''].join('');
+  const whileTap = whileTapAnimation ? { scale: fullWidth ? 0.97 : 0.9 } : undefined;
   return (
-    <motion.button whileTap={{ scale: fullWidth ? 0.97 : 0.9 }} className={buttonClassNames}>
+    <motion.button type={type} onClick={onClick} whileTap={whileTap} className={buttonClassNames}>
       {children}
     </motion.button>
   );
