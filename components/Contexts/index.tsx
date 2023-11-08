b668a655c7cc871f691cae53c8010dc6610d0e0b
Alexandr Romanchuk <alexandr.romanchuk96@gmail.com>
2023-11-08 21:24:27 +0200

    Added PageTransitionProvider

--- components/Contexts/index.tsx
+++ components/Contexts/index.tsx
@@ -1,8 +1,13 @@
 import { ReactNode } from 'react';
 import { DiscussFormVisibilityProvider } from '@/components/Contexts/DiscussFormVisibility';
+import { PageTransitionProvider } from '@/components/Contexts/PageTransition';
 
 const AppContexts = ({ children }: { children: ReactNode }) => {
-  return <DiscussFormVisibilityProvider>{children}</DiscussFormVisibilityProvider>;
+  return (
+    <DiscussFormVisibilityProvider>
+      <PageTransitionProvider>{children}</PageTransitionProvider>
+    </DiscussFormVisibilityProvider>
+  );
 };
 
 export default AppContexts;
